27f2b637fd2ab56ee1c23cd0cfed5831061a1dea
Daniel Baker <dbaker@vistaprint.com>
2022-06-10 12:03:53 +0200

    feat: remove default styles

--- __tests__/index.test.tsx
+++ __tests__/index.test.tsx
@@ -2,13 +2,13 @@ import { render, screen } from '@testing-library/react'
 import Home from '@/pages/index'
 
 describe('Home', () => {
-  it('renders a heading', () => {
+  it('renders a single h1 heading', () => {
     render(<Home />)
 
-    const heading = screen.getByRole('heading', {
-      name: /welcome to next\.js!/i,
+    const heading = screen.getAllByRole('heading', {
+      level: 1
     })
 
-    expect(heading).toBeInTheDocument()
+    expect(heading).toHaveLength(1)
   })
 })
